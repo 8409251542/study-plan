a2290f3c19269393262162c1308763d620e9d5ea
Ravi Shankar <ravi.yoof@gmail.com>
2024-05-31 21:28:28 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const addNotes=()=>{
+  const [todo,setTodo]=useState("");
+  const [hours,setHours]=useState(0);
 
+  const [list,setList]=useState([]);
+  const addNotes=(e)=>{
+    
   }
 
   return (
     <>
       <div className="main">
         <form className='border-black' action="">
-          <input type="text" />
-          <input className='' type="number" />
-          <input onClick={()=>addNotes()} type="submit" />
+          <input type="text" value={todo} onChange={(e)=>setTodo(e.target.value)}/>
+          <input className='' type="number" value={hours} onChange={(e)=>setHours(e.target.value)}/>
+          <input type="submit" />
         </form>
       </div>
       <div className="items">
